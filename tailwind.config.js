7b0fbcd2d1b7350c75f07d111e55af8e9c6ec8bc
jmano@norwexic.com <jmano@norwexic.com>
2023-06-13 16:04:16 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}',],
+  theme: {
+    extend: {scale:["group-hover"],
+    fontSize:{xxl:'9rem',
+    xxxl:'10rem',
+    xxxxl:'11rem',
+  }, 
+    colors:{
+      primary:'#71DBD4',
+      secondary:{
+        100:'#FFD319',
+        200:'#FF901F',
+      },
+      synth:{
+        orange:'#FF6C11',
+        red:'#FF3864',
+        aqua:'#2DE2E6',
+        darkblue:'#261447 ',
+        darkerblue:'#0D0221',
+        pink:'#F6019D',
+        fuschia:'#C724B1',
+      },},
+      letterSpacing:{
+        big:'.75rem'
+      },
+    fontFamily:{
+      vcr:['vcr','sans-serif'],
+      pacman:['pacman','sans-serif'],
+      outrun:['outrun','sans-serif'],
+      brat:['brat','sans-serif'],
+      lazer84:['lazer84','sans-serif'],
+      indelible:['indelible','sans-serif'],
+      roadrage:['roadrage','sans-serif'],
+      streamster:['streamster','sans-serif'],
+      commando:['commando','sans-serif'],
+      flashback:['flashback','sans-serif'],
+      rocket:['rocket','sans-serif'],
+      edit:['edit','sans-serif'],
+    }},
+  },
+  plugins: [],
+}
+
